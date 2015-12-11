58bb57f5be7110b7fc92cb1e92f63c616d7cce6c
Rustam Mingazov <rustam.mingazov@livngroup.com>
2015-12-11 12:46:24 +1100

    JMX managed beans

--- src/main/java/com/livngroup/gds/service/WexService.java
+++ src/main/java/com/livngroup/gds/service/WexService.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jmx.export.annotation.ManagedResource;
 
 import com.aocsolutions.encompasswebservices.PurchaseLogServiceStub;
 import com.livngroup.gds.domain.WexUser;
 import com.livngroup.gds.domain.WexUserToken;
 
+@ManagedResource
 public abstract class WexService {
 	
 	final protected Logger logger = LoggerFactory.getLogger(this.getClass());
