bc21ce3f4933d9ddc43ce74f6dad6f9c101a8b3e
Michael Park <michael.park@livngroup.com>
2015-12-14 13:30:55 +1100

    Service level unit-test

---
+++ src/test/java/com/livngroup/gds/service/WexPurchaseLogServiceTest.java
@@ -0,0 +1,37 @@
+package com.livngroup.gds.service;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.livngroup.gds.LivnDemoApplication;
+import com.livngroup.gds.response.CallResponse;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LivnDemoApplication.class)
+@WebIntegrationTest("server.port=8080")
+public class WexPurchaseLogServiceTest {
+
+	@Mock
+    private WexPurchaseLogService wexPurchaseLogServiceMock;
+
+    @Before
+    public void doSetup() {
+    	MockitoAnnotations.initMocks(this);    	
+    }
+
+    @Test
+	public void testPaymentService() throws Exception {
+    	try {
+    		CallResponse result = wexPurchaseLogServiceMock.queryPurchaseLog("1234", "1234567", "COMMIT");
+	        assertNull(result.getResult());
+    	} catch(Exception e) {}
+	}
+}
