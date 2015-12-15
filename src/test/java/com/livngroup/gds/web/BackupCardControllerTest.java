808f7ec14d2ff7bb8c2857cafdeecde836e8807c
Rustam Mingazov <rustam.mingazov@livngroup.com>
2015-12-15 12:09:12 +1100

    unit test fix

--- src/test/java/com/livngroup/gds/web/BackupCardControllerTest.java
+++ src/test/java/com/livngroup/gds/web/BackupCardControllerTest.java
@@ -14,14 +14,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.livngroup.gds.LivnDemoApplication;
 import com.livngroup.gds.response.CallResponse;
@@ -31,9 +28,6 @@ import com.livngroup.gds.service.WexBackupCardService;
 @SpringApplicationConfiguration(classes = LivnDemoApplication.class)
 @WebAppConfiguration
 public class BackupCardControllerTest {
-	
-	@Autowired 
-	private WebApplicationContext webApplicationContext; 
 	
 	@Mock(name = "wexBackupCardService")
 	private WexBackupCardService backupCardService;
