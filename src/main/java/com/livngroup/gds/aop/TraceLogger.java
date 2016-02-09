85f48105762bdad7579fcb2495a7a199c3a7063d
Michael Park <michael.park@livngroup.com>
2016-02-10 10:24:46 +1100

    Add tracing arguments

--- src/main/java/com/livngroup/gds/aop/TraceLogger.java
+++ src/main/java/com/livngroup/gds/aop/TraceLogger.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,21 +19,27 @@ import com.livngroup.gds.response.CallResponse;
 public class TraceLogger {
 
 	final protected Logger logger = LoggerFactory.getLogger(this.getClass());
+	final static Gson gson = new GsonBuilder().create();
 
 	@AfterReturning(
 			pointcut="execution(public * com.livngroup.gds.service.Wex*.*(..))",
 			returning="result")
 	public void traceWexApiCall(JoinPoint joinPoint, Object result) {
-		if (logger.isInfoEnabled() && (result instanceof CallResponse) ) {
-			Gson gson = new GsonBuilder().create();
+		if(logger.isInfoEnabled() && (result instanceof CallResponse) ) {
 			LocalDateTime now = LocalDateTime.now();
+			StringBuffer inputArgs = new StringBuffer();
+			
+			for(Object arg : joinPoint.getArgs()) {
+				inputArgs.append(gson.toJson(arg));
+			}
 			
 			String formatTimestamp = "[" + now +"] ";
 			CallResponse callResoponse = (CallResponse) result;
 			
-			logger.info("{} {} Call Result : {}", 
+			logger.info("{} {} Call({}) Result : {}", 
 					formatTimestamp, 
-					joinPoint.toString(), 
+					joinPoint.toString(),
+					inputArgs.toString(),
 					callResoponse == null ? "EMPTY" : gson.toJson(callResoponse));
 		}
 	}
